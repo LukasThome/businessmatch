56d2261ac69776d6366599fc341cc4948995135b
Mateus Pereira <mateus.sle.per@hotmail.com>
2023-09-18 22:19:59 -0300

    Setup básico do flask

---
+++ app.py
@@ -0,0 +1,10 @@
+# Comando para rodar o servidor com reload automático: python -m flask --debug run
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+ app.run()
