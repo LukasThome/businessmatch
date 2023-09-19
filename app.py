042ddd8d781c1f74589b79a25b61b24286dc633b
Mateus Pereira <mateus.sle.per@hotmail.com>
2023-09-18 22:54:50 -0300

    Modificações no header

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
+
+@app.route("/cadastro-empresa")
+def cadastro_empresa():
+    return render_template("cadastroEmpresa.html")
+
+@app.route("/cadastro-startup")
+def cadastro_startup():
+    return render_template("cadastroStartup.html")
 
 if __name__ == "__main__":
  app.run()
