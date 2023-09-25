3a623937cef912d7a59be175248c40608ffd15db
unknown <mateus.sle.per@hotmail.com>
2023-09-25 09:19:39 -0300

    Getters e Setters do portfolio

--- src/model/Portfolio.py
+++ src/model/Portfolio.py
@@ -6,7 +6,39 @@ data = today.strftime("%d/%m/%Y")
 
 class Portfolio:
     def __init__(self, id, nome, descricao):
-        self.id = id
-        self.nome = nome
-        self.data = data
-        self.descricao = descricao
+        self.__id = id
+        self.__nome = nome
+        self.__data = data
+        self.__descricao = descricao
+
+    @property
+    def id(self):
+        return self.__id
+    
+    @id.setter
+    def id(self, id):
+        self.__id = id
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def data(self):
+        return self.__data
+    
+    @data.setter
+    def data(self, data):
+        self.__data = data
+
+    @property
+    def descricao(self):
+        return self.__descricao
+    
+    @descricao.setter
+    def descricao(self, descricao):
+        self.__descricao = descricao
