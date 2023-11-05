ec639035ee3cd568906f605ae0aec2600aede92f
lukasthome <thome_lucas@live.com>
2023-11-05 20:57:39 -0300

    aceitar proposta

--- templates/js/proposta.js
+++ templates/js/proposta.js
@@ -114,6 +114,9 @@ function logList() {
   });
 }
 
+
+
+
 function generateEditTemplate(titulo, descricao, id) {
   let propostaCardEdit = `
         <form onsubmit="onEdit(event)" id="edit">
@@ -151,5 +154,81 @@ function remove() {
         .getElementById(`propostaId__${id}`)
         .addEventListener("click", listener);
     });
+  });
+}
+
+function aceitar(id) {
+  // Aqui você deve implementar a lógica para aceitar a proposta com o ID fornecido
+  // Você pode chamar uma função do servidor Python usando Eel para executar essa ação
+  eel.aceitarProposta(id)();
+  location.reload(); // Recarregue a página após a ação ser concluída
+}
+
+function recusar(id) {
+  // lógica para recusar a proposta com o ID fornecido
+  // chama uma função do servidor Python usando Eel para executar essa ação
+  eel.recusarProposta(id)();
+  location.reload(); // Recarregaa página após a ação ser concluída
+}
+
+function logListWithActions() {
+  var list = eel.sendPropostaList()();
+  list.then((l) => {
+    if (!l) {
+      let propostaCard = `<div>Não há propostas cadastradas!</div>`;
+      document
+        .getElementById("logBtn")
+        .insertAdjacentHTML("afterend", propostaCard);
+      return;
+    }
+    l.map(
+      ({ titulo, data, hora, descricao, id, nome_empresa, nome_startup }) => {
+        let propostaCard = `
+          <div class="card" id="cardId__${id}">
+            <div class="titleContainer">
+              <h2 id="title">Nome: ${titulo}</h2>
+              <svg id="propostaId__${id}" onclick="remove()" class="svgIcons w-6 h-6" width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" fill="#a00" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+              </svg>
+              <button class="aceitar-btn" onclick="aceitar(${id})">Aceitar</button>
+              <button class="recusar-btn" onclick="recusar(${id})">Recusar</button>
+            </div>
+            <div class="bodyText">
+              <span 
+                >Data:
+                <span id="data" >${data}</span>
+              </span>
+            </div>
+            <div class="bodyText">
+              <span
+                >Hora:
+                <span id="hora">${hora}</span>
+              </span>
+            </div>
+            <div class="descricao">
+              <span
+                >Descrição:
+                <span id="descricao">${descricao}</span>
+              </span>
+            </div>
+            <div class="nome_empresa">
+              <span
+                >Nome da Empresa:
+                <span id="nome_empresa">${nome_empresa}</span>
+              </span>
+            </div>
+            <div class="nome_startup">
+              <span
+                >Nome da Startup:
+                <span id="nome_empresa">${nome_startup}</span>
+              </span>
+            </div>
+          </div>
+        `;
+        document
+          .getElementById("logBtn")
+          .insertAdjacentHTML("afterend", propostaCard);
+      }
+    );
   });
 }
