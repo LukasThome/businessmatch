09ca4074e7936cc955d138eda9cd606984d4019e
lukasthome <thome_lucas@live.com>
2023-11-05 21:21:33 -0300

    proposta deletando

--- templates/js/proposta.js
+++ templates/js/proposta.js
@@ -143,34 +143,45 @@ function remove() {
   var list = eel.sendPropostaList()();
   list.then((l) => {
     l.map(({ id }) => {
-      const listener = () => {
-        eel.removeProposta(id)();
-        location.reload();
-        document
-          .getElementById(`propostaId__${id}`)
-          .removeEventListener("click", listener);
-      };
-      document
-        .getElementById(`propostaId__${id}`)
-        .addEventListener("click", listener);
+      const propostaId = document.getElementById(`propostaId__${id}`);
+      if (propostaId) {
+        const listener = () => {
+          eel.removeProposta(id)();
+          location.reload();
+          propostaId.removeEventListener("click", listener);
+        };
+        propostaId.addEventListener("click", listener);
+      }
     });
   });
 }
 
 function aceitar(id) {
-  // Aqui você deve implementar a lógica para aceitar a proposta com o ID fornecido
-  // Você pode chamar uma função do servidor Python usando Eel para executar essa ação
-  eel.aceitarProposta(id)();
-  location.reload(); // Recarregue a página após a ação ser concluída
+  var list = eel.sendPropostaList()();
+  list.then((propostas) => {
+    if (propostas) {
+      const proposta = propostas.find((p) => p.id === id);
+      if (proposta) {
+        proposta.status = 1; // Define o status como 1 (ou qualquer outro valor que represente "Aceitar")
+        eel.updateProposta(proposta)(); // Chama uma função no servidor Python para atualizar a proposta
+        location.reload(); // Recarrega a página após a ação ser concluída
+      }
+    }
+  });
 }
-
-function recusar(id) {
-  // lógica para recusar a proposta com o ID fornecido
-  // chama uma função do servidor Python usando Eel para executar essa ação
-  eel.recusarProposta(id)();
-  location.reload(); // Recarregaa página após a ação ser concluída
-}
-
+  function recusar(id) {
+    var list = eel.sendPropostaList()();
+    list.then((propostas) => {
+      if (propostas) {
+        const proposta = propostas.find((p) => p.id === id);
+        if (proposta) {
+          proposta.status = 0; // Define o status como 0 (ou qualquer outro valor que represente "Recusar")
+          eel.updateProposta(proposta)(); // Chama uma função no servidor Python para atualizar a proposta
+          location.reload(); // Recarrega a página após a ação ser concluída
+        }
+      }
+    });
+  }
 function logListWithActions() {
   var list = eel.sendPropostaList()();
   list.then((l) => {
@@ -188,8 +199,9 @@ function logListWithActions() {
             <div class="titleContainer">
               <h2 id="title">Nome: ${titulo}</h2>
               <svg id="propostaId__${id}" onclick="remove()" class="svgIcons w-6 h-6" width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" fill="#a00" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+              <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
               </svg>
+
               <button class="aceitar-btn" onclick="aceitar(${id})">Aceitar</button>
               <button class="recusar-btn" onclick="recusar(${id})">Recusar</button>
             </div>
