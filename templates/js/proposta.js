687336ef399086a67cffc54c048b96a25caadc89
Mateus Pereira <mateus.sle.per@hotmail.com>
2023-11-05 21:35:43 -0300

    Alteração mínima no proposta.js

--- templates/js/proposta.js
+++ templates/js/proposta.js
@@ -1,4 +1,4 @@
-var form = document.getElementById("signup");
+var form = document.getElementById('signup');
 var currentDatetime = new Date();
 
 // Função para adicionar uma proposta
@@ -6,37 +6,37 @@ async function onSubmit(event) {
   event.preventDefault();
   const currentSession = await eel.currentSession()();
   var proposta = {
-    nome_empresa: "Nome estático",
+    nome_empresa: 'Nome estático',
     nome_startup: currentSession.nome,
-    titulo: form.elements["titulo"].value,
+    titulo: form.elements['titulo'].value,
     data: formatDate(currentDatetime), // Get and format the current date
     hora: formatTime(currentDatetime), // Get and format the current time
-    descricao: form.elements["descricao"].value,
+    descricao: form.elements['descricao'].value,
   };
 
   eel.workWithValuesProposta(proposta)();
   console.log(proposta.value);
-  console.log("create");
+  console.log('create');
 
   location.reload();
 }
 
 // Deixar a data como YYYY-MM-DD
 function formatDate(date) {
-  var day = String(date.getDate()).padStart(2, "0");
+  var day = String(date.getDate()).padStart(2, '0');
   var monthNames = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
   ];
   var month = monthNames[date.getMonth()];
   var year = date.getFullYear();
@@ -45,9 +45,9 @@ function formatDate(date) {
 
 // Deixar a hora como HH:MM:SS
 function formatTime(date) {
-  var hours = String(date.getHours()).padStart(2, "0");
-  var minutes = String(date.getMinutes()).padStart(2, "0");
-  var seconds = String(date.getSeconds()).padStart(2, "0");
+  var hours = String(date.getHours()).padStart(2, '0');
+  var minutes = String(date.getMinutes()).padStart(2, '0');
+  var seconds = String(date.getSeconds()).padStart(2, '0');
   return `${hours}:${minutes}:${seconds}`;
 }
 
@@ -57,8 +57,8 @@ function logList() {
     if (!l) {
       let propostaCard = `<div>Não há propostas cadastradas!</div>`;
       document
-        .getElementById("logBtn")
-        .insertAdjacentHTML("afterend", propostaCard);
+        .getElementById('logBtn')
+        .insertAdjacentHTML('afterend', propostaCard);
       return;
     }
     console.log(l);
@@ -107,15 +107,12 @@ function logList() {
                 </div>
             `;
         document
-          .getElementById("logBtn")
-          .insertAdjacentHTML("afterend", propostaCard);
+          .getElementById('logBtn')
+          .insertAdjacentHTML('afterend', propostaCard);
       }
     );
   });
 }
-
-
-
 
 function generateEditTemplate(titulo, descricao, id) {
   let propostaCardEdit = `
@@ -148,9 +145,9 @@ function remove() {
         const listener = () => {
           eel.removeProposta(id)();
           location.reload();
-          propostaId.removeEventListener("click", listener);
+          propostaId.removeEventListener('click', listener);
         };
-        propostaId.addEventListener("click", listener);
+        propostaId.addEventListener('click', listener);
       }
     });
   });
@@ -165,7 +162,7 @@ function aceita(id) {
         proposta.status = 1; // Define o status como 1 (ou qualquer outro valor que represente "Aceitar")
         eel.updateProposta(proposta)(); // Chama uma função no servidor Python para atualizar a proposta
         location.reload(); // Recarrega a página após a ação ser concluída
-        console.log
+        console.log;
       }
     }
   });
@@ -177,8 +174,8 @@ function logListProposta() {
     if (!l) {
       let propostaCard = `<div>Não há propostas cadastradas!</div>`;
       document
-        .getElementById("logBtn")
-        .insertAdjacentHTML("afterend", propostaCard);
+        .getElementById('logBtn')
+        .insertAdjacentHTML('afterend', propostaCard);
       return;
     }
     l.map(
@@ -191,7 +188,7 @@ function logListProposta() {
               <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
               </svg>
 
-              <button class="aceitar-btn" onclick="aceita(${id})">Aceitar</button>
+              <button class="aceitar-btn" onclick="aceita('${id}')">Aceitar</button>
             </div>
             <div class="bodyText">
               <span 
@@ -226,8 +223,8 @@ function logListProposta() {
           </div>
         `;
         document
-          .getElementById("logBtn")
-          .insertAdjacentHTML("afterend", propostaCard);
+          .getElementById('logBtn')
+          .insertAdjacentHTML('afterend', propostaCard);
       }
     );
   });
